f2b0a6d7b5b466d674f2979f29e9b905dbf02c0a
Kaan Yeşillik <mkaanyesillik@gmail.com>
2021-09-05 00:23:27 +0300

    BruteForceGenerator
    basic program for string value permutation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BruteForceKeyGenerator
 {
-    partial class Form1
+    partial class BruteForceGenerator
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -28,32 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BruteForceGenerator));
             this.txtChars = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblChars = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.btnCreate = new System.Windows.Forms.Button();
-            this.txtMin = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtMax = new System.Windows.Forms.TextBox();
+            this.lblAciklama = new System.Windows.Forms.Label();
+            this.lblToplamCikti = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtChars
             // 
-            this.txtChars.Location = new System.Drawing.Point(114, 35);
+            this.txtChars.Location = new System.Drawing.Point(113, 9);
             this.txtChars.Name = "txtChars";
             this.txtChars.Size = new System.Drawing.Size(158, 20);
             this.txtChars.TabIndex = 0;
             // 
-            // label1
+            // lblChars
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Harfleri Girin :";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblChars.AutoSize = true;
+            this.lblChars.Location = new System.Drawing.Point(11, 12);
+            this.lblChars.Name = "lblChars";
+            this.lblChars.Size = new System.Drawing.Size(70, 13);
+            this.lblChars.TabIndex = 1;
+            this.lblChars.Text = "Harfleri Girin :";
+            this.lblChars.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // backgroundWorker1
             // 
@@ -61,62 +60,45 @@
             // 
             // btnCreate
             // 
-            this.btnCreate.Location = new System.Drawing.Point(197, 226);
+            this.btnCreate.Location = new System.Drawing.Point(14, 119);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.Size = new System.Drawing.Size(258, 23);
             this.btnCreate.TabIndex = 2;
             this.btnCreate.Text = "Oluştur";
             this.btnCreate.UseVisualStyleBackColor = true;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
-            // txtMin
+            // lblAciklama
             // 
-            this.txtMin.Location = new System.Drawing.Point(114, 61);
-            this.txtMin.Name = "txtMin";
-            this.txtMin.Size = new System.Drawing.Size(158, 20);
-            this.txtMin.TabIndex = 3;
+            this.lblAciklama.Location = new System.Drawing.Point(12, 32);
+            this.lblAciklama.Name = "lblAciklama";
+            this.lblAciklama.Size = new System.Drawing.Size(259, 71);
+            this.lblAciklama.TabIndex = 7;
+            this.lblAciklama.Text = resources.GetString("lblAciklama.Text");
             // 
-            // label2
+            // lblToplamCikti
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 64);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Min Kaç Karakter :";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblToplamCikti.AutoSize = true;
+            this.lblToplamCikti.Location = new System.Drawing.Point(12, 103);
+            this.lblToplamCikti.Name = "lblToplamCikti";
+            this.lblToplamCikti.Size = new System.Drawing.Size(71, 13);
+            this.lblToplamCikti.TabIndex = 8;
+            this.lblToplamCikti.Text = "Toplam Çıktı :";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 90);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Max Kaç Karakter :";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // txtMax
-            // 
-            this.txtMax.Location = new System.Drawing.Point(114, 87);
-            this.txtMax.Name = "txtMax";
-            this.txtMax.Size = new System.Drawing.Size(158, 20);
-            this.txtMax.TabIndex = 5;
-            // 
-            // Form1
+            // BruteForceGenerator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtMax);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtMin);
+            this.ClientSize = new System.Drawing.Size(284, 151);
+            this.Controls.Add(this.lblToplamCikti);
+            this.Controls.Add(this.lblAciklama);
             this.Controls.Add(this.btnCreate);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblChars);
             this.Controls.Add(this.txtChars);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "BruteForceGenerator";
+            this.Text = "BruteForceGenerator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,13 +107,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtChars;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblChars;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button btnCreate;
-        private System.Windows.Forms.TextBox txtMin;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label lblAciklama;
+        private System.Windows.Forms.Label lblToplamCikti;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace BruteForceKeyGenerator
 {
-    public partial class Form1 : Form
+    public partial class BruteForceGenerator : Form
     {
 
-        public Form1()
+        public BruteForceGenerator()
         {
             InitializeComponent();
-
+            Control.CheckForIllegalCrossThreadCalls = false;
         }
+        int sayac = 0;
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
@@ -26,7 +22,53 @@ namespace BruteForceKeyGenerator
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
+            String str = txtChars.Text.ToString();
+            int n = str.Length;
+            permute(str, 0, n - 1);
+            MessageBox.Show("Tüm Kombinasyonlar Dosyada.","BruteForceGenerator",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            sayac = 0;
+        }
+        public void permute(String str,
+                                int l, int r)
+        {
+            string dosya_yolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)+"\\dosya.txt";
+            FileStream fs = new FileStream(dosya_yolu, FileMode.Append, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            if (l == r) { 
+              
+            sw.WriteLine(str);
+                sayac++;
+                lblToplamCikti.Text = "Toplam Çıktı :" + sayac.ToString();
 
+
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+        public static String swap(String a,
+                              int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sayac = 0;
         }
     }
 }
